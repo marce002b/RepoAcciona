d2299ec1e768dff0712cb1909878382702d77f76
Administrador <Administrador@DESKTOP-86108N3>
2021-01-27 12:39:11 -0300

    simple controller logi simulated

---
+++ ApiProvinciasEjer/ProjApiProvinciasEjer/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjApiProvinciasEjer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        // GET api/Login
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return  "Debe hacer un POST a Api/Login para continuar";
+        }
+
+        // POST api/Login
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+
+    }
+}
