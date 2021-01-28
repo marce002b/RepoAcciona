10ff417aa1a6340fc0a27e9f5ec5a45aa28352b7
Administrador <Administrador@DESKTOP-86108N3>
2021-01-28 12:01:46 -0300

    esto es para que no salga null  y salgan corchete []  cada vex que no hay data, hago un ctor con la lista vacia

--- ApiProvinciasEjer/ProjApiProvinciasEjer/Models/Pais.cs
+++ ApiProvinciasEjer/ProjApiProvinciasEjer/Models/Pais.cs
@@ -8,6 +8,12 @@ namespace ProjApiProvinciasEjer.Models
 {
     public class Pais
     {
+
+        public Pais()
+        {
+            Provincias = new List<Provincia>(); //esto es para que no salga null , y salgan corchete [] cada vex que no hay data, hago un ctor con la lista vacia
+
+        }
         public int Id { get; set; }
         ///cmj en caso de largo nombre "Nombre_Pcia": [ "The field Nombre_Pcia must be a string with a maximum length of 45."
 
--- ApiProvinciasEjer/ProjApiProvinciasEjer/Models/Provincia.cs
+++ ApiProvinciasEjer/ProjApiProvinciasEjer/Models/Provincia.cs
@@ -10,6 +10,11 @@ namespace ProjApiProvinciasEjer.Models
 {
     public class Provincia
     {
+
+        public Provincia()
+        {
+            Municipios = new List<Municipio>(); //esto es para que no salga null  y salgan corchete []  cada vex que no hay data, hago un ctor con la lista vacia
+        }
         public int Id { get; set; }
         [StringLength(45)]
         public string Nombre_Pcia { get; set; }//cmj en caso de largo nombre "Nombre_Pcia": [ "The field Nombre_Pcia must be a string with a maximum length of 45."
